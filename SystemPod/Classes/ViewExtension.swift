16078087161a4d75f5fca6094c668f9e1a749ee0
Harsh.Joshi <harsh.joshi@IB-SEZ-LAP-472.local>
2023-06-30 13:06:26 +0530

    color extnsion added

---
+++ SystemPod/Classes/ViewExtension.swift
@@ -0,0 +1,14 @@
+//
+//  ViewExtension.swift
+//  SystemPod
+//
+//  Created by Harsh.Joshi on 30/06/23.
+//
+
+import Foundation
+extension UIView {
+    //New code for repleace
+    public func addBackgroundPodColor() {
+        layer.backgroundColor = UIColor.cyan.cgColor
+    }
+}
